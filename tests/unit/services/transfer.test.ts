0abcf3704df67d538e2e07d44de7817b7bc76e6c
Matheus Martinez <decretomusic@gmail.com>
2023-02-09 17:10:53 -0300

    Inicial commit

---
+++ tests/unit/services/transfer.test.ts
@@ -0,0 +1,12 @@
+describe('Deveria criar uma transferência TRIX', function () {
+  it('Deveria criar uma transferência TRIX com SUCESSO', async function () {
+    // Arrange
+    // Act
+    // Assert
+  });
+  it('Deveria lançar uma exceção quando a key é inválida', async function () {
+    // Arrange
+    // Act
+    // Assert
+  });
+});
